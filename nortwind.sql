1a2df5dbc8261e495ab839d438f2c2c811430f3f
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-01-14 23:32:40 -0600

    Update nortwind.sql

--- nortwind.sql
+++ nortwind.sql
@@ -25,8 +25,6 @@ DROP TABLE IF EXISTS customer_demographics;
 DROP TABLE IF EXISTS employee_territories;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS shippers;
-DROP TABLE IF EXISTS suppliers;
 DROP TABLE IF EXISTS territories;
 DROP TABLE IF EXISTS us_states;
 DROP TABLE IF EXISTS categories;
